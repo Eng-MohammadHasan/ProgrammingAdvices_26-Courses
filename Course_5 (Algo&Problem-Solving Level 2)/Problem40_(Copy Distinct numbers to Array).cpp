a2b5aaf325b3c27123c6d1775fcd4b052f819d91
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-07-08 22:45:55 +0300

    Update Problem40_(Copy Distinct numbers to Array).cpp

--- Course_5 (Algo&Problem-Solving Level 2)/Problem40_(Copy Distinct numbers to Array).cpp
+++ Course_5 (Algo&Problem-Solving Level 2)/Problem40_(Copy Distinct numbers to Array).cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void FillArray(int arr[100] , int &arrayLength)
+{
+	arrayLength = 10;
+	//(Hard-coded) :-)
+	arr[0] = 10;
+	arr[1] = 10;
+	arr[2] = 10;
+	arr[3] = 50;
+	arr[4] = 50;
+	arr[5] = 70;
+	arr[6] = 70;
+	arr[7] = 70;
+	arr[8] = 70;
+	arr[9] = 90;
+}
+
 void AddArrayElement(int Number, int arr[100], int& arrayLength)
 {
 	arrayLength++;
@@ -43,19 +59,10 @@ void PrintArray(int arr[100], int arrayLength)
 
 int main()
 {
-	int arr[10], arr2[10], arrayLength = 10, arr2Length = 0;
-	//(Hard-coded) :-)
-	arr[0] = 10;
-	arr[1] = 10;
-	arr[2] = 10;
-	arr[3] = 50;
-	arr[4] = 50;
-	arr[5] = 70;
-	arr[6] = 70;
-	arr[7] = 70;
-	arr[8] = 70;
-	arr[9] = 90;
+	int arr[10], arr2[10], arrayLength = 0, arr2Length = 0;
 
+	FillArray(arr,arrayLength);
+	
 	CopyDistinctNumbers(arr, arr2, arrayLength, arr2Length);
 
 	cout << "\nArray 1 elements : \n";
