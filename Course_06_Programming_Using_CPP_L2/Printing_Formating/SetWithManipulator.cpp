64fe78f4735308bea0c8a7e357dd50edc31b8d66
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-19 09:37:49 +0300

    Update SetWithManipulator.cpp

--- Course_06_Programming_Using_CPP_L2/Printing_Formating/SetWithManipulator.cpp
+++ Course_06_Programming_Using_CPP_L2/Printing_Formating/SetWithManipulator.cpp
@@ -7,7 +7,7 @@ int main()
 	cout << "---------|--------------------------------|---------|" << endl;
 	cout << "   Code  |              Name              |  Mark   |" << endl;
 	cout << "---------|--------------------------------|---------|" << endl;
-	cout << setw(9) << "C101" << "|" << setw(32) << "introduction to programming 1" << "|" << setw(9) << "95" << "|" << endl;
+	cout << left << setw(9) << "C101" << "|" << setw(32) << "introduction to programming 1" << "|" << setw(9) << "95" << "|" << endl;
 	cout << setw(9) << "C102" << "|" << setw(32) << "Computer Hardware" << "|" << setw(9) << "88" << "|" << endl;
 	cout << setw(9) << "C10352" << "|" << setw(32) << "Network" << "|" << setw(9) << "75" << "|" << endl;
 	cout << "---------|--------------------------------|---------|" << endl;
