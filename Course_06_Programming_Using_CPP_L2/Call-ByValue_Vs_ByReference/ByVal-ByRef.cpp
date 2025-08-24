e6cfaadc909f716f3fb95877e0e3f41eb60cbf32
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-24 22:18:50 +0300

    .,.

---
+++ Course_06_Programming_Using_CPP_L2/Call-ByValue_Vs_ByReference/ByVal-ByRef.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void Function(int x)
+{
+	x++;
+	cout << "x address ... " << &x << endl;
+}
+
+void Function2(int& x)
+{
+	x++;
+	cout << "x address ... " << &x << endl;
+}
+
+int main()
+{
+	int a = 10;
+
+	cout << a << endl;
+	cout << &a << endl;
+
+	Function(a);
+	cout << a << endl;
+	cout << "a adress : " << &a << endl;
+
+	Function2(a);
+	cout << a << endl;
+	cout << "a adress : " << &a << endl;
+	return 0;
+}
