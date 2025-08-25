ad1e6c6f695ba3dc2542f51676118acf3ac5a7b3
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-25 11:49:01 +0300

    Update Create-Reference.cpp

--- Course_06_Programming_Using_CPP_L2/Pointer/Create-Reference.cpp
+++ Course_06_Programming_Using_CPP_L2/Pointer/Create-Reference.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 int main()
 {
 	int a = 10;
@@ -11,12 +12,24 @@ int main()
 	cout << &x << endl;
 	cout << &y << endl;
 
+	cout << "a = " << a << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << endl;
+
 	x = 20;
-	
-	cout << a << endl;
-	cout << x << endl;
-	cout << y << endl;
+
+	cout << "a = " << a << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << endl;
+
+	a = 30;
+
+	cout << "a = " << a << endl;
+	cout << "x = " << x << endl;
+	cout << "y = " << y << endl;
+	cout << endl;
 
 	return 0;
 }
-
