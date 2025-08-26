8353a20547972c1c8fc5981bfcb6c41efc6d2d91
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-26 15:13:00 +0300

    Create Reference-Vs-Pointer.cpp

---
+++ Course_06_Programming_Using_CPP_L2/Pointer/Reference-Vs-Pointer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 10;
+	int& x = a; // Another name (alias) for the variable a , for the same memory slot!
+
+	cout << &a << endl;
+	cout << &x << endl;
+
+	cout << a << endl;
+	cout << x << endl;
+
+	int* p = &a;
+
+	cout << p << endl;
+	cout << *p << endl;
+
+	int b = 20;
+	p = &b;
+
+	cout << p << endl;
+	cout << *p << endl;
+
+	return 0;
+
+}
