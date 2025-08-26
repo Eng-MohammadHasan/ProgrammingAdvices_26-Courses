e1863bf57eaeb1075b83683e5e35ce1789dd8c89
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-26 22:25:31 +0300

    Create Pointers-and-Arrays.cpp

---
+++ Course_06_Programming_Using_CPP_L2/Pointers-And-References/Pointers-and-Arrays.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[4] = { 10 , 20 , 30 ,40 };
+	int* ptr;
+	ptr = arr;
+
+	//ptr is equivalent to &arr[0]
+	//ptr + 1 is equivalent to &arr[1]
+	//ptr + 2 is equivalent to &arr[2]
+	//ptr + 3 is equivalent to &arr[3]
+
+	/*for (int& Items : arr)
+	{
+		cout << &Items << endl;
+	}
+	cout << endl << endl;*/
+
+	cout << "Addresses are :\n";
+	cout << ptr << endl;
+	cout << ptr + 1 << endl;
+	cout << ptr + 2 << endl;
+	cout << ptr + 3 << endl;
+
+	//cout << "\nValues are: \n";
+	cout << *(ptr) << endl;
+	cout << *(ptr + 1) << endl;
+	cout << *(ptr + 2) << endl;
+	cout << *(ptr + 3) << endl;
+
+	// or like this ...
+	cout << endl;
+
+	for (int i = 0; i < 4; i++)
+	{
+		cout << *(ptr + i) << endl;
+	}
+
+	cout << arr << endl;
+	cout << &arr[0] << endl;
+	cout << &arr << endl;
+	return 0;
+}
