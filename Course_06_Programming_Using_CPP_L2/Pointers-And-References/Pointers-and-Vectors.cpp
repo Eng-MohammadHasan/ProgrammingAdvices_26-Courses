320f212ff6ea283e28483482a21e0e0ce9c8f1ed
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-08-27 14:25:12 +0300

    Create Pointers-and-Vectors.cpp

---
+++ Course_06_Programming_Using_CPP_L2/Pointers-And-References/Pointers-and-Vectors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+
+  *ptr holds the address of the vector object itself, not its elements. 
+  
+  *The vector object contains metadata (size, capacity, pointer to the actual array).
+  
+  *The first element of the vector is somewhere else, inside the dynamically allocated array.
+  
+*/
+int main() {
+   
+  vector<int> v = {10, 20, 30};
+
+    vector<int>* ptr = &v;   // pointer to vector
+
+    cout << (*ptr)[1] << endl;       // access with []
+    cout << ptr->at(2) << endl;      // access with ->
+
+    return 0;
+  
+}
