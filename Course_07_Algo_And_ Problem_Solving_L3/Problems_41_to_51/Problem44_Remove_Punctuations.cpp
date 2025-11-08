be9fa99923cbf3efce20713324d78cc5167dd31a
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2025-11-08 22:57:42 +0300

    Add function to remove punctuations from a string

---
+++ Course_07_Algo_And_ Problem_Solving_L3/Problems_41_to_51/Problem44_Remove_Punctuations.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+string RemovePunctuations(string S1)
+{
+	short pos;
+
+	for (short i = 0; i < S1.length(); i++)
+	{
+		if (ispunct(S1[i]))
+		{
+			S1 = S1.erase(i, 1);
+		}
+	}
+
+	return S1;
+}
+
+int main()
+{
+	string S1 = "Welcome to Jordan, Jordan is a nice country: it's amazing.";
+
+	cout << "\nOriginal String\n" << S1;
+
+	cout << "\n\nPunctuations Removed:\n";
+	cout << RemovePunctuations(S1) << endl;
+	cout << endl;
+
+	system("pause");
+}
